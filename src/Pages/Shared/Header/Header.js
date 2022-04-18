412a1e2bdac8e7d87b3cd19e5eea7cf36b7a5665
sabujmahonto <sabujmahonto@gmail.com>
2022-04-18 10:31:02 +0600

    Header componet or Footer component added

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,43 @@
+import { signOut } from 'firebase/auth';
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
+
+
+const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () =>{
+        signOut(auth);
+    }
+
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" sticky='top' bg="primary" variant="dark">
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="home#services">Services</Nav.Link>
+                            <Nav.Link as ={Link} to="checkout">Checkout</Nav.Link>
+                            <Nav.Link as={Link} to="about">About</Nav.Link>
+                            {
+                                user ?
+                                    <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>sign out</button>
+                                :
+                                <Nav.Link as={Link} to="login">
+                                Login
+                            </Nav.Link>}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
