1c27c5a0d866d1e6ce3897c245487592caf2f05c
sabujmahonto <sabujmahonto@gmail.com>
2022-04-18 21:50:05 +0600

    upddate process.env

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -15,6 +15,10 @@ const About = () => {
              Hello! I'm Sabuj Mahonto,I develop web applications, My core skill
               is based on JavaScript and I love to do most of the things using
               JavaScript. I love to make the web more open to the world.
+
+              <br/>
+              <p>My goal is to build myself as a skilled full stack web developer before 2023.I will just keep learning whenever I have time to meet my goals And I will make something every day.In the midst of hundreds of activities, I will set aside a minimum of eight hours per day for learning.No matter how many obstacles come, I will not back down from my goal</p>
+              
              </div>
             </Card.Text>
           </Card.Body>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey:process.envREACT_APP_apiKey,
-    authDomain:process.envREACT_APP_authDomain,
-    projectId:process.envREACT_APP_projectId,
-    storageBucket:process.envREACT_APP_storageBucket,
-    messagingSenderId:process.envREACT_APP_messagingSenderId,
-    appId:process.envREACT_APP_appId,
+    apiKey:process.env.REACT_APP_apiKey,
+    authDomain:process.env.REACT_APP_authDomain,
+    projectId:process.env.REACT_APP_projectId,
+    storageBucket:process.env.REACT_APP_storageBucket,
+    messagingSenderId:process.env.REACT_APP_messagingSenderId,
+    appId:process.env.REACT_APP_appId,
 
   };
 
