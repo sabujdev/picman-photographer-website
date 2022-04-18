1267b94c80fe858c844484a1bdb338e18345290c
sabujmahonto <sabujmahonto@gmail.com>
2022-04-18 11:16:19 +0600

    banner images added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@
 import './App.css';
 import Header from './Pages/Shared/Header/Header';
 import Footer from './Pages/Shared/Header/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div >
       <Header></Header>
-      
+      <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/home" element={<Home></Home>}></Route>
+      </Routes>
+
       <Footer></Footer>
 
     </div>
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+
+
+
+import banner4 from '../../../images/banner/banner4.jpg'
+import banner7 from '../../../images/banner/banner7.jpg'
+import banner9 from '../../../images/banner/banner9.jpg'
+
+const Banner = () => {
+    const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+    return (
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner4}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner7}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner9}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+};
+
+export default Banner;
