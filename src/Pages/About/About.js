e3e20097054007fca1ee277de1d89cf9602fef37
sabujmahonto <sabujmahonto@gmail.com>
2022-04-18 19:17:41 +0600

    About or blogs added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,27 @@
-import React from 'react';
-
+import React from "react";
+import { Card, CardGroup } from "react-bootstrap";
+import img from "../../images/Profile.jpg";
+import "./About.css";
 const About = () => {
-    return (
-        <div>
-            <h1>This is About me</h1>
-        </div>
-    );
+  return (
+    <div className="profile">
+      <CardGroup>
+        <Card>
+          <Card.Img variant="top" src={img} style={{ width: "18rem" }} />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+             <div>
+             Hello! I'm Sabuj Mahonto,I develop web applications, My core skill
+              is based on JavaScript and I love to do most of the things using
+              JavaScript. I love to make the web more open to the world.
+             </div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </CardGroup>
+    </div>
+  );
 };
 
 export default About;
--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card, Col, Row } from "react-bootstrap";
+import {  Card, Col, Row } from "react-bootstrap";
 
 import "./Blogs.css";
 
